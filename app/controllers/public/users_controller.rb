e58e8561e8c781a9813b2de57ca4f8b147033c42
YukiYoshizawa <eatermgsorigin4@gmail.com>
2025-01-22 08:37:36 +0000

    [update]users/edit

--- app/controllers/public/users_controller.rb
+++ app/controllers/public/users_controller.rb
@@ -9,16 +9,23 @@ class Public::UsersController < ApplicationController
   end
 
   def edit
-
+    @user = User.find(current_user.id)
   end
 
   def update
-
+    @user = User.find(current_user.id)
+    if @user.update(user_params)
+      flash[:notice] = "ユーザーデータを更新しました"
+      redirect_to users_my_page_path
+    else
+      flash.now[:notice] = "エラー"
+      render :edit
+    end
   end
 
   private
 
   def user_params
-
+    params.require(:user).permit(:name, :is_active, :profile_image)
   end
 end
