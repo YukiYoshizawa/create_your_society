b042708a43722919e7847629ebad767f636d1d44
YukiYoshizawa <eatermgsorigin4@gmail.com>
2025-01-23 08:53:03 +0000

    [add] admin/users/function

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,6 +1,32 @@
 class Admin::UsersController < ApplicationController
 
   def index
-    
+    @users = User.all
+  end
+
+  def show
+    @user = User.find(params[:id])
+    @societies = @user.societies
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      flash[:notice] = "ユーザーデータを更新しました"
+      redirect_to admin_users_show_path(@user.id)
+    else
+      flash.now[:notice] = "エラー"
+      render :edit
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :is_active, :profile_image)
   end
 end
