f31776c8fb938e3de2538e3b227063e8660211f1
YukiYoshizawa <eatermgsorigin4@gmail.com>
2025-02-19 11:36:04 +0000

    [update] page

--- app/controllers/public/homes_controller.rb
+++ app/controllers/public/homes_controller.rb
@@ -1,7 +1,7 @@
 class Public::HomesController < ApplicationController
 
   def top
-    @societies = Society.all
+    @societies = Society.all.page(params[:page])
     # @user = User.find(current_user.id)
   end
 end
